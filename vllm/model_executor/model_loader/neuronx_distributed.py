8aa2bfe264bfd768a9b32e6574f99d692f391deb
EmilyWebber <egwebber@amazon.com>
2025-02-18 20:28:18 +0000

    Adding imports

--- vllm/model_executor/model_loader/neuronx_distributed.py
+++ vllm/model_executor/model_loader/neuronx_distributed.py
@@ -3,7 +3,6 @@ import importlib
 import hashlib
 import os
 import shutil
-from typing import Dict, List, Optional, Tuple
 
 import torch
 import torch.nn as nn
@@ -21,6 +20,37 @@ from vllm.sequence import (CompletionSequenceGroupOutput, Logprob,
 from neuronx_distributed_inference.models.mllama.utils import create_vision_mask
 from neuronx_distributed_inference.utils.hf_adapter import load_pretrained_config
 from neuronx_distributed_inference.models.config import FusedSpecNeuronConfig, OnDeviceSamplingConfig
+
+from functools import cached_property
+
+from .vllm_data import TokenInputs, token_inputs
+
+
+from vllm.attention import AttentionMetadata
+from vllm.distributed import divide, get_tensor_model_parallel_world_size
+from vllm.inputs import INPUT_REGISTRY,InputContext
+
+from vllm.model_executor.layers.layernorm import RMSNorm
+from vllm.model_executor.layers.linear import (MergedColumnParallelLinear,
+                                               QKVParallelLinear,
+                                               RowParallelLinear)
+from vllm.model_executor.layers.quantization import QuantizationConfig
+from vllm.model_executor.layers.sampler import SamplerOutput
+
+from vllm.model_executor.model_loader.weight_utils import default_weight_loader
+from vllm.model_executor.sampling_metadata import SamplingMetadata
+from vllm.multimodal import MULTIMODAL_REGISTRY
+
+from vllm.multimodal.utils import cached_get_tokenizer
+from vllm.sequence import IntermediateTensors, SequenceData
+
+from typing import (TYPE_CHECKING, Any, Callable, Mapping, NamedTuple,
+                    Optional, Protocol, Union, Dict, List, Tuple,
+                    Type, Iterable,  Mapping, Set)
+
+DecoderOnlyInputs = Union[TokenInputs, "MultiModalInputs"]
+
+NestedTensors = Union[list["NestedTensors"], list[torch.Tensor], torch.Tensor, tuple[torch.Tensor, ...]]
 
 logger = init_logger(__name__)
 
@@ -52,7 +82,167 @@ _NEURON_SUPPORTED_MODELS: Dict[str, Tuple[str, str]] = {
                          "NeuronPixtralForConditionalGeneration"),
 }
 
-class NeuronPixtralConditionalGeneration(nn.Module):
+
+
+class DummyData(NamedTuple):
+    """Dummy data used for profiling."""
+
+    seq_data: "SequenceData"
+    multi_modal_data: Optional["MultiModalDataDict"] = None
+    multi_modal_placeholders: Optional["MultiModalPlaceholderDict"] = None
+
+def get_rmsnorm_cls():
+    # Initialize to the appropriate implementation of RMSNorm
+    # If infer on NXD -> CustomRMSNorm
+    # If infer on CPU -> HF's LlamaRMSNorm (CustomRMSNorm does not work on CPU)
+    return CustomRMSNorm if parallel_state.get_tensor_model_parallel_size() > 1 else LlamaRMSNorm
+
+
+def make_empty_intermediate_tensors(batch_size: int,
+                                    dtype: torch.dtype,
+                                    device: torch.device,
+                                    ) -> IntermediateTensors:
+    return IntermediateTensors({
+        key:
+        torch.zeros((batch_size, hidden_size), dtype=dtype, device=device)
+        for key in keys
+    })
+
+
+def get_max_pixtral_image_tokens(ctx: InputContext):
+    tokenizer = cached_get_tokenizer(
+        ctx.model_config.tokenizer,
+        tokenizer_mode=ctx.model_config.tokenizer_mode)
+    mm_encoder = tokenizer.instruct.mm_encoder
+
+    image_config = mm_encoder.mm_config if hasattr(
+        mm_encoder, "mm_config") else mm_encoder.image_config
+
+    max_image_size = image_config.max_image_size
+    image_patch_size = image_config.image_patch_size
+
+    return ((max_image_size // image_patch_size)**2)
+
+
+def dummy_data_for_pixtral(ctx: InputContext, seq_len: int,
+                           mm_counts: Mapping[str, int]):
+    tokenizer = cached_get_tokenizer(
+        ctx.model_config.tokenizer,
+        tokenizer_mode=ctx.model_config.tokenizer_mode)
+
+    mm_encoder = tokenizer.mistral.instruct_tokenizer.mm_encoder
+    image_token_id = mm_encoder.special_ids.img
+
+    mm_config = ctx.get_mm_config()
+    num_images = mm_config.limit_per_prompt.get("image", 1)
+
+    # dummy size
+    size = 256
+    image = Image.new("RGB", (size, size), color=0)
+
+    encoding = tokenizer.instruct.mm_encoder(ImageChunk(image=image))
+    image_feature_size = len(encoding.tokens)
+    num_image_tokens = image_feature_size * num_images
+    seq_data = SequenceData.from_prompt_token_counts(
+        (image_token_id, num_image_tokens),
+        (0, seq_len - num_image_tokens),
+    )
+
+    mm_data = {"image": num_images * [image]}
+    mm_placeholders = {
+        "image":
+        consecutive_placeholder_ranges(num_items=num_images,
+                                       item_size=image_feature_size)
+    }
+    return DummyData(seq_data, mm_data, mm_placeholders)
+
+
+def input_mapper_for_pixtral(ctx: InputContext, data: object):
+    """Maps the input data to its MultiModalKwargs (if any).
+
+    Args:
+        ctx: Context of the loaded model.
+        data: data potentially containing PIL images to be processed
+            and mapped to `images`.
+
+    Returns:
+        MultiModalKwargs containing the stacked normalized images tensor or
+        image embeddings.
+    """
+    model_config = ctx.model_config
+    tokenizer = cached_get_tokenizer(
+        model_config.tokenizer, tokenizer_mode=model_config.tokenizer_mode)
+
+    data_list = data if isinstance(data, list) else [data]
+
+    images = []
+    image_tokens_list = []
+    for image_data in data_list:
+        image = ImageChunk(image=image_data)
+        encoding = tokenizer.instruct.mm_encoder(image)
+        image = torch.from_numpy(encoding.image).to(dtype=torch.float16)
+        images.append(image)
+        image_tokens_list.append(encoding.tokens)
+
+    image_tokens = torch.tensor([
+        token_id for image_tokens in image_tokens_list
+        for token_id in image_tokens
+    ])
+
+    # return MultiModalKwargs({"images": images, "image_tokens": image_tokens})
+
+    rt = {"images": images, "image_tokens": image_tokens}
+
+    return rt    
+
+
+def input_processor_for_pixtral(ctx: InputContext, inputs: DecoderOnlyInputs):
+    multi_modal_data = inputs.get("multi_modal_data")
+    if multi_modal_data is None or "image" not in multi_modal_data:
+        return inputs
+
+    prompt_token_ids = inputs.get("prompt_token_ids")
+    prompt = inputs.get("prompt")
+    tokenizer = cached_get_tokenizer(
+        ctx.model_config.tokenizer,
+        tokenizer_mode=ctx.model_config.tokenizer_mode)
+
+    mm_encoder = tokenizer.mistral.instruct_tokenizer.mm_encoder
+    image_token_id = mm_encoder.special_ids.img
+    image_break_id = mm_encoder.special_ids.img_break
+    image_end_id = mm_encoder.special_ids.img_end
+
+    if image_token_id not in inputs['prompt_token_ids']:
+        raise ValueError(
+            f"You've passed {inputs=} without {image_token_id=}"
+            " Make sure to process your input via mistral_common's"
+            " tokenizer or pass a chat completion request. For more"
+            " For more info, see: "
+            "https://github.com/vllm-project/vllm/issues/8411.")
+
+    # Get precise tracking of placeholder positions
+    placeholder_ranges = []
+    curr_offset = -1
+    curr_length = 0
+    for i in range(len(prompt_token_ids)):
+        if prompt_token_ids[i] in (image_token_id, image_break_id):
+            if curr_offset < 0:
+                curr_offset = i
+            curr_length += 1
+        elif prompt_token_ids[i] == image_end_id:
+            curr_length += 1
+            placeholder_ranges.append(
+                PlaceholderRange(offset=curr_offset, length=curr_length))
+            curr_offset = -1
+            curr_length = 0
+        else:
+            pass
+    return token_inputs(prompt=prompt,
+                        prompt_token_ids=prompt_token_ids,
+                        multi_modal_data=multi_modal_data,
+                        multi_modal_placeholders={"image": placeholder_ranges})
+
+class NeuronPixtralForConditionalGeneration(nn.Module):
     def __init__(
         self,
         config: PretrainedConfig,
@@ -686,8 +876,9 @@ def get_neuron_model(model_config: ModelConfig,
     if model_arch == "MllamaForConditionalGeneration":
         model = NeuronMllamaForCausalLM(model_config.hf_config)
 
-    elif model_arch == "PixtralForConditionalGeneration"
+    elif model_arch == "PixtralForConditionalGeneration":
         model = NeuronPixtralForConditionalGeneration(model_config.hf_config)
+        
     else:
         model = NeuronCasualLM(model_config.hf_config)
     default_neuron_config_args = _get_default_neuron_config(
